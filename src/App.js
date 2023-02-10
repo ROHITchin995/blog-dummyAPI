d637386162ef76d56b3141700e95994fb730dd22
ROHITchin995 <rohitsure.9915@gmail.com>
2023-02-10 13:33:31 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { PostPage } from './pages/PostPage';
+import { Error } from './pages/Error';
 function App() {
   return (
-    <div className="App">
-      Blog App
-    </div>
+   <Router>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/postpage" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<Error />} /> 
+    </Routes>
+   </Router>
   );
 }
 
