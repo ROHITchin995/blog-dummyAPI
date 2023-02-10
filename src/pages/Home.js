0e9ed0c6ffb01f89ea3fce87ef4ec938bfe60416
ROHITchin995 <rohitsure.9915@gmail.com>
2023-02-10 19:43:29 +0530

    momentjs added for date format

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import '../App.css'
+var moment = require('moment');
 export const Home = () => {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(5);
   const [posts, setPosts] = useState();
-
+ 
 
   useEffect(() => {
     getPost()
@@ -25,56 +26,41 @@ export const Home = () => {
 
   }
 
+  const postClick = (index)=>{
+    console.log(index)
+  }
+
   return (
   <div className='min-h-screen'>
     <div className='container py-12'>
     <div className='mb-16'>
-      <div class="grid gap-4 grid-cols-1 grid-cols-2">
-        <div class="bg-white rounded shadow p-4 overflow-hidden">
-          <div class="flex mb-4 items-center"><img src="https://randomuser.me/api/portraits/med/men/51.jpg" class="rounded-full w-10 mr-4" alt="mr. Jozef Van Miert" />
-            <div>mr. Jozef Van Miert<br /><small>Jun 26 2022 18:55:07</small></div>
+      <div className="grid gap-4 grid-cols-1 grid-cols-2">
+      { posts && 
+   posts.data.map((post, index)=>(
+    <div key={index} className="bg-white rounded shadow p-4 overflow-hidden post-outer" onClick={()=>postClick(post.id)}>
+          <div className="flex mb-4 items-center"><img src={post.owner.picture} className="rounded-full w-10 mr-4" alt={post.owner.firstName} />
+            <div>{post.owner.title}. {post.owner.firstName} {post.owner.lastName}<br /><small>{moment(post.publishDate).format('MMMM DD YYYY HH:mm:ss')}</small></div>
           </div>
-          <div class="grid grid-cols-2 gap-4">
-            <div class="self-center"><img src="https://img.dummyapi.io/photo-1568480541687-16c2f73eea4c.jpg" class="object-cover w-full h-60" alt="hello oikm " /></div>
-            <div class="self-center"><small class="mb-4">Jun 26 2022 18:55:07</small>
-              <div class="mb-4">hello oikm </div>
-              <div class="flex flex-wrap text-xs mb-4">
-                <div class="px-2 py-1 border mr-1 mb-1 rounded bg-pink-600 text-white">hello</div>
-                <div class="px-2 py-1 border mr-1 mb-1 rounded bg-pink-600 text-white">hello world</div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="self-center"><img src={post.image} className="object-cover w-full h-60" alt={post.text} /></div>
+            <div className="self-center"><small className="mb-4">{moment(post.publishDate).format('MMMM DD YYYY HH:mm:ss')}</small>
+              <div className="mb-4">{post.text} </div>
+              <div className="flex flex-wrap text-xs mb-4">
+                {
+                  post.tags.map((tag, index)=>(
+                    <div key={index} className="px-2 py-1 border mr-1 mb-1 rounded bg-pink-600 text-white">{tag}</div>
+                  ))
+                }
               </div>
-              <div><img src="/img/like.svg" class="w-5 mr-2 float-left" alt="like" />3</div>
+              <div><img src="https://dummyapi.io/img/like.svg" className="w-5 mr-2 float-left" alt="like" />{post.likes}</div>
             </div>
           </div>
         </div>
+   )
+   )
+    }
+        
       </div>
-
-
-
-
-      {posts &&
-        posts.data.map((post, index) => (
-          <div key={index} className="post">
-            <div>
-              <img className='owner-image' src={post.owner.picture} alt={post.owner.firstName} />
-              {post.owner.title}.{post.owner.firstName}
-              <div>
-                {post.publishDate.substring(0, 10)} {post.publishDate.substring(11, 19)}
-              </div>
-            </div>
-            <img className='post-image' src={post.image} alt={post.text} />
-            <div>
-              {post.text}
-            </div>
-            <div>
-              {post.tags}
-            </div>
-            <div>
-              {post.likes}
-            </div>
-          </div>
-        )
-        )
-      }
     </div>
     </div>
     </div>
